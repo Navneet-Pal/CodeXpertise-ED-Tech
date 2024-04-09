21fabbe172dd326ca92b587e08fe21cdb812fa67
Navneet-Pal <navneetpal303@gmail.com>
2024-04-09 18:30:55 +0530

    seventh commit

--- mpServer/controllers/profile.js
+++ mpServer/controllers/profile.js
@@ -150,12 +150,15 @@ exports.getEnrolledCourses = async (req, res) => {
 			},
 		})
         .exec()
+		
 		// userDetails = userDetails.toObject()
 		var SubsectionLength = 0
 		for (var i = 0; i < userDetails.courses.length; i++) {
+			
 		  let totalDurationInSeconds = 0
 		  SubsectionLength = 0
 		  for (var j = 0; j < userDetails.courses[i].courseContent.length; j++) {
+			
 			totalDurationInSeconds += userDetails.courses[i].courseContent[
 			  j
 			].subSection.reduce((acc, curr) => acc + parseInt(curr.timeDuration), 0)
@@ -165,10 +168,13 @@ exports.getEnrolledCourses = async (req, res) => {
 			SubsectionLength +=
 			  userDetails.courses[i].courseContent[j].subSection.length
 		  }
+		  
 		  let courseProgressCount = await CourseProgress.findOne({
 			courseID: userDetails.courses[i]._id,
 			userId: userId,
 		  })
+		  
+		
 		  courseProgressCount = courseProgressCount?.completedVideos.length
 		  if (SubsectionLength === 0) {
 			userDetails.courses[i].progressPercentage = 100
@@ -181,7 +187,7 @@ exports.getEnrolledCourses = async (req, res) => {
 			  ) / multiplier
 		  }
 		}
-
+	
       if (!userDetails) {
         return res.status(400).json({
           success: false,
