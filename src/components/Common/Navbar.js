2fe8927091cdcf4fcd01e186b171a3cb0584cbe8
Navneet-Pal <navneetpal303@gmail.com>
2024-04-09 18:49:04 +0530

    navbar

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -80,7 +80,7 @@ function Navbar() {
                                                 <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
                                                 {loading ? (
                                                     <p className="text-center">Loading...</p>
-                                                ): subLinks.length ? (
+                                                ): subLinks?.length ? (
                                                             subLinks.map( (subLink, index) => (
                                                                 <Link to={`/catalog/${subLink.name.split(" ").join("-").toLowerCase()}`} key={index}>
                                                                     <p>{subLink.name}</p>
